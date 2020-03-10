94eae92fa91e44de2add0dd7a965a519e6e2b87e
Rigo <Alerson.Rigo@br.experian.com>
2020-03-10 19:03:22 -0300

    Swagger to generate classes from yaml

---
+++ src/main/java/com/swagger/generatorswagger/GeneratorSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.swagger.generatorswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swagger/generatorswagger/GeneratorSwaggerApplicationTests.java
@@ -0,0 +1,14 @@
+
+package com.swagger.generatorswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
